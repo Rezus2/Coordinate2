75e895700ccc39ccbd9b2ab414a1071a25a6171c
Rezus <rere.ddd1112@yandex.ru>
2019-09-28 11:44:54 +0300

    coordinate2

---
+++ src/com/company/coordinate2.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class coordinate2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        if ((-3 <= x && x <= 5) || (9 <= x && x <= 15))
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
